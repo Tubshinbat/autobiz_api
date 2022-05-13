062c6b7ec999bb5e7fd9a366e23f24b435f73234
tuvshinbat <tuvshinbat596@gmail.com>
2022-05-13 13:57:05 +0800

    product search

--- controller/Product.js
+++ controller/Product.js
@@ -57,6 +57,10 @@ exports.getProducts = asyncHandler(async (req, res, next) => {
 
     const carZagvar = await CarZagvar.find({ name: { $regex: term } });
     const carIndustry = await CarIndustry.find({ name: { $regex: term } });
+
+    console.log(carZagvar);
+    console.log(carIndustry);
+
     query.find({
       $or: [
         {
