1e81220cb69677ab46d0a14d24f0d9602705b093
tuvshinbat <tuvshinbat596@gmail.com>
2022-05-16 02:43:14 +0800

    news get

--- controller/News.js
+++ controller/News.js
@@ -148,6 +148,8 @@ exports.updateNews = asyncHandler(async (req, res, next) => {
     req.body.categories = [req.body.categories];
   }
 
+  req.body.updateUser = req.userId;
+
   news = await News.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
