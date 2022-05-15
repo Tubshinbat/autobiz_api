95d886eb31ab8de8f5ecc089d2e705d3e64c2aa7
tuvshinbat <tuvshinbat596@gmail.com>
2022-05-16 03:13:45 +0800

    user

--- controller/Users.js
+++ controller/Users.js
@@ -162,7 +162,7 @@ exports.getUsers = asyncHandler(async (req, res, next) => {
         { username: { $regex: ".*" + name + ".*", $options: "i" } },
         { lastname: { $regex: ".*" + name + ".*", $options: "i" } },
         { email: { $regex: ".*" + name + ".*", $options: "i" } },
-        { phone: parseInt(name) },
+        { phone: parseInt(name) || "" },
       ],
     });
   query.select(select);
