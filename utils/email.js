f1009ee94934f116fda8177cbf339b4da92d7db8
tuvshinbat <tuvshinbat596@gmail.com>
2022-05-17 00:03:48 +0800

    user

--- utils/email.js
+++ utils/email.js
@@ -1,13 +1,22 @@
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 
+const CLIENT_ID =
+  "779701740791-jrt4achvh9qibsgteht0k01pqvneremi.apps.googleusercontent.com";
+const CLEINT_SECRET = "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";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN =
+  "1//04ZRcrJlcdlafCgYIARAAGAQSNwF-L9IrDzzty7Ls2CokLf_hIZewTQht-6gP1mEkmecu84zM5MDRRMeBy61LmMvY_aaeobylxcg";
+const SMTP_FROM = "Autobiz.mn";
+const SMTP_FROM_EMAIL = "autobizotp@gmail.com";
+
 const oAuth2Client = new google.auth.OAuth2(
-  process.env.CLIENT_ID,
-  process.env.CLEINT_SECRET,
-  process.env.REDIRECT_URI
+  CLIENT_ID,
+  CLEINT_SECRET,
+  REDIRECT_URI
 );
 
-oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
 const sendEmail = async (options) => {
   try {
@@ -17,14 +26,14 @@ const sendEmail = async (options) => {
       auth: {
         type: "OAuth2",
         user: "autobizotp@gmail.com",
-        clientId: process.env.CLIENT_ID,
-        refreshToken: process.env.REFRESH_TOKEN,
+        clientId: CLIENT_ID,
+        refreshToken: REFRESH_TOKEN,
         accessToken: accessToken,
       },
     });
 
     const mailOptions = {
-      from: `${process.env.SMTP_FROM} <${process.env.SMTP_FROM_EMAIL}>`,
+      from: `${SMTP_FROM} <${SMTP_FROM_EMAIL}>`,
       to: options.email, // list of receivers
       subject: options.subject, // Subject line
       html: options.message, // html body
@@ -42,18 +51,18 @@ const sendEmail = async (options) => {
 // const sendEmail = async (options) => {
 //   // create reusable transporter object using the default SMTP transport
 //   let transporter = nodemailer.createTransport({
-//     host: process.env.SMTP_HOST,
-//     port: process.env.SMTP_PORT,
+//     host: SMTP_HOST,
+//     port: SMTP_PORT,
 //     secure: true, // true for 465, false for other ports
 //     auth: {
-//       user: process.env.SMTP_USERNAME, // generated ethereal user
-//       pass: process.env.SMTP_PASSWORD, // generated ethereal password
+//       user: SMTP_USERNAME, // generated ethereal user
+//       pass: SMTP_PASSWORD, // generated ethereal password
 //     },
 //   });
 
 //   // send mail with defined transport object
 //   let info = await transporter.sendMail({
-//     from: `${process.env.SMTP_FROM} <${process.env.SMTP_FROM_EMAIL}>`, // sender address
+//     from: `${SMTP_FROM} <${SMTP_FROM_EMAIL}>`, // sender address
 //     to: options.email, // list of receivers
 //     subject: options.subject, // Subject line
 //     html: options.message, // html body
