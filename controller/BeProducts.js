39269ddd16e04bf2ca44412a3a77fa968c7d1e90
Tubshinbat <tuvshinbat596@gmail.com>
2022-07-04 21:04:36 +0800

    rest

--- controller/BeProducts.js
+++ controller/BeProducts.js
@@ -117,8 +117,8 @@ exports.getBeProducts = asyncHandler(async (req, res) => {
   if (valueRequired(priceText)) query.where("price").equals(priceText);
 
   if (valueRequired(fuel)) {
-    if (fuel === "Hybrid(Petrol)") fuel = "Hybrid (Petrol)";
-    if (fuel === "Hybrid(Diesel)") fuel = "Hybrid";
+    if (fuel === "Hybrid(Petrol)") fuel = "Hybrid Petrol";
+    if (fuel === "Hybrid(Diesel)") fuel = "Hybrid Diesel";
     console.log(fuel);
 
     query.find({ fuel: { $regex: ".*" + fuel + ".*", $options: "i" } });
